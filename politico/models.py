aa6094c4537dc5895fb38f47ad436630f1d7961a
ickyatcity <shiroshana@gmail.com>
2020-05-10 19:36:24 +0100

    politico app create

--- politico/models.py
+++ politico/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Candidate(models.Model):
+    cadidate = models.CharField(max_length=225)
+    cadidate_desc = models.CharField(max_length=225)
+    url_wiki = models.CharField(max_length=255, blank=True, null=True)
+    title_wiki = models.CharField(max_length=255, blank=True, null=True)
+    content_wiki = models.TextField(blank=True, null=True)
